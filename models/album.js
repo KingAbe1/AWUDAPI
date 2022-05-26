6aa8c6fd3c3d0e358c2de47338a9dcdaf337dca3
abeni16 <abeni3ase7@gmail.com>
2022-05-26 19:11:30 +0300

    all in one

--- models/album.js
+++ models/album.js
@@ -1,21 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const albumSchema = new mongoose.Schema({
-    album_title:
-    {
-    type:String,
-    required:true
-    },
-     artist_name:{
-     type:String,
-     required:true
-    },
-    album_description:{
-    type:String,
-    },
-    album_image:{
-    type:String,
-    required:true
-    }
-})
-module.exports = mongoose.model('Album',albumSchema )
+  album_title: {
+    type: String,
+    required: true,
+  },
+  artist_name: {
+    type: String,
+    required: true,
+  },
+  album_description: {
+    type: String,
+  },
+  album_image: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = mongoose.model("Album", albumSchema);
--- models/music.js
+++ models/music.js
@@ -1,34 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const musicSchema = new mongoose.Schema({
-    title:
-    {
-        type:String,
-        required:true
-},
-   artist_name:{
-    type:String,
-    required:true
-},
-music_description:{
-    type:String,
-    required:true
-},
-image:{
-    type:String,
-    required:true
-},
-path:{
-    type:String,
-    required:true
-},
-album:{
-    type:String,
-
-},
-rate:{
-    type:'number',
-    
-}
-})
-module.exports = mongoose.model('Music',musicSchema )
+  title: {
+    type: String,
+    required: true,
+  },
+  artist_name: {
+    type: String,
+    required: true,
+  },
+  music_description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  path: {
+    type: String,
+    required: true,
+  },
+  album: {
+    type: String,
+  },
+  rate: {
+    type: "number",
+  },
+});
+module.exports = mongoose.model("Music", musicSchema);
